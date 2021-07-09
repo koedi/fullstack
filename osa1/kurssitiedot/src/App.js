8585047d0e345e0bb03424156683c181b7ccd050
koedi <kari.pietikainen@gmail.com>
2021-07-09 14:16:30 +0300

    ex 1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -2,26 +2,27 @@ import React from "react"
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+  ]
 
   return (
   <div>
       <Header course={course}/>
 
-      <Content parts={[part1, part2, part3]} />
+      <Content parts={parts} />
 
-      <Total totalCount={part1.exercises + part2.exercises + part3.exercises} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -52,7 +53,7 @@ const Total = (props) => {
   console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.totalCount}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   )
 
