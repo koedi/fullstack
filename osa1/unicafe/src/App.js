320466b0d2946d1195407bb0331a0b1e8f8b41e2
koedi <kari.pietikainen@gmail.com>
2021-07-10 15:04:29 +0300

    ex 1.11*

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -8,33 +8,35 @@ const App = () => {
 
   return (
     <div>
-      <Header />
-      <Button name="good" good setValue={() => setGood(good + 1)} />
-      <Button name="neutral" neutral setValue={() => setNeutral(neutral + 1)}/>
-      <Button name="bad" bad setValue={() => setBad(bad + 1)}/>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
-    </div>
-  )
-}
-
-
-
-const Header = () => {
-  return (
-    <div>
       <h1>Give feedback</h1>
+      <table>
+        <tbody>
+        <tr>
+          <td>
+            <Button name="good" good setValue={() => setGood(good + 1)} />
+          </td>
+          <td>
+            <Button name="neutral" neutral setValue={() => setNeutral(neutral + 1)} />
+          </td>
+          <td>
+            <Button name="bad" bad setValue={() => setBad(bad + 1)} />
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-const Button = ({name, count, setValue}) => {
+
+
+const Button = ({ name, count, setValue }) => {
   return (
-    <div>
-      <button onClick= {setValue}>
+    <button onClick={setValue}>
       {name}
-      </button>
-      <p>{count}</p>
-    </div>
+    </button>
   )
 }
 
@@ -42,32 +44,34 @@ const Statistics = ({ good, neutral, bad }) => {
   const sum = good + neutral + bad
   if (sum === 0) {
     return (
-      <div>
-        <h1>Statistics</h1>
         <p>No feedback given</p>
-      </div>
+    )
+  } else {
+    return (
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={good} />
+            <StatisticsLine text="neutral" value={neutral} />
+            <StatisticsLine text="bad" value={bad} />
+            <StatisticsLine text="all" value={sum} />
+            <StatisticsLine text="average" value={((1 * good + 0 * neutral + -1 * bad) / sum).toFixed(2)} />
+            <StatisticsLine text="positive" value={((good / sum) * 100).toFixed(2)} sign="%" />
+          </tbody>
+        </table>
     )
   }
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <p>
-        <StatisticsLine text="good" value={good} />
-        <StatisticsLine text="neutral" value={neutral} />
-        <StatisticsLine text="bad" value={bad} />
-        <StatisticsLine text="all" value={sum} />
-        <StatisticsLine text="average" value={(1 * good + 0 * neutral + -1 * bad) / sum} />
-        <StatisticsLine text="positive" value={(good / sum)*100} sign="%"/>
-      </p>
-    </div>
-  )
 }
 
-const StatisticsLine = ({text, value, sign}) => {
+const StatisticsLine = ({ text, value, sign }) => {
   return (
-    <div>
-      {text} {value} {sign}<br/>
-    </div>
+      <tr>
+        <td>
+          {text}
+        </td>
+        <td>
+          {value} {sign}
+        </td>
+      </tr>
   )
 }
 
