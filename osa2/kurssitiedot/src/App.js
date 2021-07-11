a560ac12b762890b1764b4df857db807f4e28d15
koedi <kari.pietikainen@gmail.com>
2021-07-11 23:27:10 +0300

    ex 2.1

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,64 @@
+import React from "react"
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+    { course.parts.map(p => <Part part={p} />) }
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+export default App;
