198b75bf9abf259616dcb68694b0006e5a8a4eb4
koedi <kari.pietikainen@gmail.com>
2021-07-13 14:57:42 +0300

    ex 2.9*

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1231244' }
+        { name: 'Arto Hellas', number: '040-1231244', show: true },
+        { name: 'Ada Lovelace', number: '39-44-5323523', show: true },
+        { name: 'Dan Abramov', number: '12-43-234345', show: true },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', show: true },
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [searchTerm, setSearchTerm] = useState('')
 
     const addInfo = (event) => {
         event.preventDefault()
@@ -25,6 +29,17 @@ const App = () => {
     }
 
 
+    const filterPersons = (event) => {
+        /** 
+         * 1) set all persons to show=false i.e. not to show
+         * 2) filter person list with search term
+         * 3) set filtered list to show=true i.e. to show
+         * 
+        */
+        persons.map(p => p.show = false)
+        const filteredPersons = persons.filter(p => (p.name.toLowerCase()).includes(event.target.value.toLowerCase()))
+        filteredPersons.map(p => p.show = true)
+    }
 
 
 
@@ -36,9 +51,21 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value)
+        filterPersons(event)
+    }
+
+
+
     return (
         <div>
             <h2>Phonebook</h2>
+                <div>
+                    filter shown with: <input value={searchTerm} onChange={handleSearchTermChange}></input>
+                </div>
+        
+            <h2>Add new number</h2>
             <form onSubmit={addInfo}>
                 <div>
                     name:   <input value={newName} onChange={handleNameChange}/><br/>
@@ -49,7 +76,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(p => <p key={p.name}> {p.name} {p.number}</p>)}            
+            {persons.filter(p => p.show === true).map(p => <p key={p.name}> {p.name} {p.number}</p>)}
         </div>
     )
 }
