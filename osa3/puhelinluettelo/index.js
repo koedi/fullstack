b7c2e5e4f27e4e5962524ffe2c8ac1fd56fe5a9e
koedi <kari.pietikainen@gmail.com>
2021-07-16 08:33:29 +0300

    ex 3.6

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/persons', (req, res) => {
-res.json(persons)
+    res.json(persons)
 })
 
 app.get('/info', (req, res) => {
@@ -58,23 +58,39 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
     const person = req.body
+
+    if (!req.body.name) {
+        console.log("app.post(): no name")
+        res.status(400).json({error: 'name is missing'})
+        return
+    }
+    if (!req.body.number) {
+        console.log("app.post(): no number")
+        res.status(400).json({error: 'number is missing'})
+        return
+    }
+    console.log(persons.some(p => p.name === req.body.name))
+    if (persons.some(p => p.name === req.body.name)) {
+        console.log("app.post(): an entry exists for: ", req.body.name)
+        res.status(400).json({error: 'an entry exists for that name'})
+        return
+    }
+
     const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0
     person.id = maxId + 1
 
     persons = persons.concat(person)
 
-
-
-    console.log(person)
+    console.log("app.post(): created new entry:", person)
     res.json(person)
-    })
+})
 
 
 
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
 
