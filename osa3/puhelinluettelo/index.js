ac7dd689ebc100f4e2aa86bfd0b79df2bd8b41dd
koedi <kari.pietikainen@gmail.com>
2021-08-12 16:52:52 +0300

    ex 3.22

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -12,94 +12,94 @@ app.use(express.json())
 app.use(cors())
 
 // configure morgan to show extra info for POST
-morgan.token('post', (request, response) => {
-    if(request.method === "POST") {
-        return JSON.stringify(request.body)
-    } else {
-        return ""
-    }
+morgan.token('post', (request) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
+  } else {
+    return ''
+  }
 })
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post"))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
-    
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 
 app.get('/info', (req, res) => {
-    Entry.find({})
-    .then(results => 
-        res.send(`<p>Phonebook has info for ${results.length} people</p> <p>${Date()}`)
-        )
+  Entry.find({})
+    .then(results =>
+      res.send(`<p>Phonebook has info for ${results.length} people</p> <p>${Date()}`)
+    )
 })
 
 app.get('/api/persons', (req, res) => {
-    Entry.find({}).then(entries => {
-        res.json(entries)
-    })
+  Entry.find({}).then(entries => {
+    res.json(entries)
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-   Entry.findById(req.params.id)
-   .then(entry => {
-        if (entry) {
-            res.json(entry)
-        } else {
-            res.status(404).end()
-        }
-   })
-   .catch(error => next(error))
+  Entry.findById(req.params.id)
+    .then(entry => {
+      if (entry) {
+        res.json(entry)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Entry.findByIdAndRemove(req.params.id)
-    .then(entry => {
-        res.status(204).end()
+  Entry.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    if (!req.body.name) {
-        return res.status(400).json({error: 'name is missing'})        
-    } else if (!req.body.number) {
-        return res.status(400).json({error: 'number is missing'})
-    } 
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'name is missing' })
+  } else if (!req.body.number) {
+    return res.status(400).json({ error: 'number is missing' })
+  }
 
-    const entry = new Entry( {
-        name: req.body.name,
-        number: req.body.number
-    })
+  const entry = new Entry({
+    name: req.body.name,
+    number: req.body.number
+  })
 
-    entry.save().then(result => {
-        res.json(entry)
-    })
+  entry.save().then(() => {
+    res.json(entry)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const entry = {
-        name: req.body.name,
-        number: req.body.number,
-    }
+  const entry = {
+    name: req.body.name,
+    number: req.body.number,
+  }
 
-    Entry.findByIdAndUpdate(req.params.id, entry, {new: true})
+  Entry.findByIdAndUpdate(req.params.id, entry, { new: true })
     .then(updatedEntry => {
-        res.json(updatedEntry)
+      res.json(updatedEntry)
     })
     .catch(error => next(error))
 })
@@ -112,7 +112,7 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
 
--- osa3/puhelinluettelo/models/entry.js
+++ osa3/puhelinluettelo/models/entry.js
@@ -4,33 +4,33 @@ const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true})
-.then(result => {
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
+  .then(() => {
     console.log('connected to MongoDB')
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     console.log('error connecting to MongoDB', error.message)
-})
+  })
 const entrySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true
-    }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  }
 })
 
 entrySchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 entrySchema.plugin(uniqueValidator)
