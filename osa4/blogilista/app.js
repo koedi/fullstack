a5f1ccf41b827bf600606bc3d9befef11713754b
koedi <kari.pietikainen@gmail.com>
2021-08-13 11:24:58 +0300

    ex 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URI
+
+
+logger.info('Connecting to', mongoUrl)
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('Error connecting to MongoDB: ', error.message)
+  })
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,48 +1,11 @@
-require('dotenv').config()
 const http = require('http')
-const mongoose = require('mongoose')
-const express = require('express')
-const cors = require('cors')
+const app = require('./app')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
-const app = express()
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const PORT = process.env.PORT
-const mongoUrl = process.env.MONGODB_URI //'mongodb://localhost/bloglist'
-
-
-const Blog = mongoose.model('Blog', blogSchema)
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => console.log(error))
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
