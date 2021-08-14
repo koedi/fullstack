8a10b26e152db79509c3d6eed7f7d4ceda794b34
koedi <kari.pietikainen@gmail.com>
2021-08-14 10:24:37 +0300

    ex 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,8 +1,20 @@
-const dummy = (blogs) => {
-  console.log(blogs)
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.size === 0 ? 0 : blogs.reduce(reducer, 0)
+}
+
+
+
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
