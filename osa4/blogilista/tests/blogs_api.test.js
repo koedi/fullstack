52318b7460f0f08a68c3d5425752a28f8e962332
koedi <kari.pietikainen@gmail.com>
2021-08-16 17:27:16 +0300

    ex 4.9*

--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -5,20 +5,30 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('babbys first async/await', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+
+  test('there are six blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(6)
+  })
+})
+
+describe('blogging is fun!', () => {
+  test('id is id not _id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
 
-test('there are six blogs', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(6)
-})
 
-
-afterAll( () => {
+afterAll(() => {
   mongoose.connection.close()
 })
