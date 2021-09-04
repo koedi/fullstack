a709d0680d959581e739f4b9893d440a1f29f42d
koedi <kari.pietikainen@gmail.com>
2021-09-04 10:33:17 +0300

    ex 5.1

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword]= useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
+
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +18,65 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with: ', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('invalid credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+
+  }
+
+  const loginForm = () => (
     <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+        </div>
+        <div>
+          password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+        </div>
+        <div>
+          <button type="submit">login</button>
+        </div>
+      </form>
+    </div>
+  )
+
+  const showBlogs = () => (
+     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
+
+  return (
+    <div>
+      {user === null && loginForm()}
+      {user !== null && showBlogs()}    
+    </div>
+
+  )
+  
+
+
+
+
 }
 
 export default App
